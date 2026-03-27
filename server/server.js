8220a2fbcbc9c348a3a0e0b329f5c28e7798d166
fanol-hcw <fanol.ismaili@stud.hcw.ac.at>
2026-03-27 17:14:25 +0100

    implemented all three parts required from the acceptance criteria

--- server/server.js
+++ server/server.js
@@ -8,9 +8,51 @@ app.use(express.static(path.join(__dirname, 'files')));
 // Configure a 'get' endpoint for data..
 app.get('/movies', function (req, res) {
   // Part 1: Remove the next line and replace with your code
-  res.send('!dlrow olleH')
-})
+  let movieArray = [
+  {
+   "Title":"Matrix",
+   "Year":"1993",
+   "Released":"1993-03-01",
+   "Runtime": 60,
+   "Genres": ["Action", "Drama", "Fantasy"],
+   "Writers":["Grenville Case"],
+   "Directors":["Brother Something", "John Cusack"],
+   "Actors":["Nick Mancuso", "Phillip Jarrett", "Carrie-Anne Moss"],
+   "Plot":"Hitman Steven Matrix is shot, experiences afterlife, gets second chance by helping others. Wakes up, meets guides assigning cases where he aids people using unorthodox methods from past profession.",
+   "Poster":"https://m.media-amazon.com/images/M/MV5BM2JiZjU1NmQtNjg1Ni00NjA3LTk2MjMtNjYxMTgxODY0NjRhXkEyXkFqcGc@._V1_SX300.jpg",
+   "Metascore": 82,
+   "imdbRating": 7.2,
+   },
+   {
+       "Title": "Inception",
+       "Released": "2010-07-16",
+       "Runtime": 148,
+       "Genres": ["Action", "Adventure", "Sci-Fi"],
+       "Directors": ["Christopher Nolan"],
+       "Writers": ["Christopher Nolan"],
+       "Actors": ["Leonardo DiCaprio", "Joseph Gordon-Levitt", "Elliot Page"],
+       "Plot": "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a CEO, but his tragic past may doom the project and his team to disaster.",
+       "Poster": "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+       "Metascore": 74,
+       "imdbRating": 8.8
+   },
+   {
+       "Title": "Shutter Island",
+       "Released": "2010-02-19",
+       "Runtime": 138,
+       "Genres": ["Drama", "Mystery", "Thriller"],
+       "Directors": ["Martin Scorsese"],
+       "Writers": ["Laeta Kalogridis", "Dennis Lehane"],
+       "Actors": ["Leonardo DiCaprio", "Emily Mortimer", "Mark Ruffalo"],
+       "Plot": "Two US marshals are sent to a mental institution on an inhospitable island in order to investigate the disappearance of a patient.",
+       "Poster": "https://m.media-amazon.com/images/M/MV5BN2FjNWExYzEtY2YzOC00YjNlLTllMTQtNmIwM2Q1YzBhOWM1XkEyXkFqcGc@._V1_SX300.jpg",
+       "Metascore": 63,
+       "imdbRating": 8.2
+   }
+]
 
+res.send(movieArray);
+});
 app.listen(3000)
 
 console.log("Server now listening on http://localhost:3000/")
